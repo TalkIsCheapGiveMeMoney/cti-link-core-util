04e6347ff913016b0c673513b99f01114d044dff
fengwei <x@vonvey.com>
2016-05-23 14:59:34 +0800

    taskScheduler增加参数返回

--- cti-link-core/src/main/java/com/tinet/ctilink/scheduler/TaskSchedulerTrigger.java
+++ cti-link-core/src/main/java/com/tinet/ctilink/scheduler/TaskSchedulerTrigger.java
@@ -1,9 +1,11 @@
 package com.tinet.ctilink.scheduler;
 
+import java.util.Map;
+
 /**
  * @author fengwei //
  * @date 16/5/19 13:04
  */
 public interface TaskSchedulerTrigger {
-    void taskTriggered(String taskId);
+    void taskTriggered(String taskId, Map param);
 }
